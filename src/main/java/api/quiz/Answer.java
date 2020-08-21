268bffb7e24fb51b1abcb1935f11b4573b7e8817
Brenner <brenner.lattin@gmail.com>
2020-08-21 00:17:09 -0600

    Fixed Answer class so that it doesn't include the quiz

--- src/main/java/api/quiz/Answer.java
+++ src/main/java/api/quiz/Answer.java
@@ -2,22 +2,16 @@ package api.quiz;
 
 public class Answer {
 
-    private Quiz quiz;
     private boolean success;
     private String feedback;
 
     public Answer(Quiz quiz, int answer) {
-        this.quiz = quiz;
         this.success = answer == 3;
         if (success) {
             this.feedback = "You won, yay";
         } else {
             this.feedback = "You lost, oh no";
         }
-    }
-
-    public Quiz getQuiz() {
-        return quiz;
     }
 
     public boolean isSuccess() {
@@ -27,6 +21,5 @@ public class Answer {
     public String getFeedback() {
         return feedback;
     }
-
 
 }
